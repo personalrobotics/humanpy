d00daf63db3c334732e650b388186b06da4c056e
stepelle <stefania.pellegrinelli@itia.cnr.it>
2016-03-28 12:54:05 -0400

    changes required by the pull request

--- src/humanpy/utils.py
+++ src/humanpy/utils.py
@@ -3,14 +3,33 @@ from tf import transformations, LookupException, ConnectivityException, Extrapol
 
 
 def constrain(x, x_min, x_max):
+    """
+    Check if a number is in the range. If not, 
+    the min or max values are returned.
+    
+    @param x value to be checked
+    @param x_min minimum range value
+    @parem x_max maximum range value
+    """
     return min(max(x, x_min), x_max)
 
 def humanInList(human, ids):
+    """
+    Check if the given human id matched one of the id in the list ids
+    
+    @param human human loaded in the env
+    @param ids list of id to be checked
+    """
     for id in ids:
         if human.id == 'user_' + id: return True
     return False
         
 def define_pose_from_eetransform(ee_pose):
+    """
+    Transform a pose in a Pose()
+    
+    @param ee_pose end effector pose
+    """
     ee_quat = transformations.quaternion_from_matrix(ee_pose)        
     pose = Pose()
     pose.position.x = ee_pose[0,3]
@@ -23,6 +42,12 @@ def define_pose_from_eetransform(ee_pose):
     return pose   
 
 def define_pose_from_posquat(pos, quat):
+    """
+    Transform a position and a quaternion in a Pose()
+    
+    @param pos x y z position in meters
+    @param quat quaternion
+    """
     pose = Pose()
     pose.position.x = pos[0]
     pose.position.y = pos[1]
@@ -34,26 +59,61 @@ def define_pose_from_posquat(pos, quat):
     return pose  
 
 def hide(env, body):
+    """
+    Hide the human in the simulation env
+    
+    @param env environment
+    @param body human representation in the env
+    """
     with env:
         body.SetVisible(False)
         body.Enable(False)
     return False
 
 def show(env, body):
+    """
+    Make visible the human in the simulation env
+    
+    @param env environment
+    @param body human representation in the env
+    """
     with env:
         body.SetVisible(True)
         body.Enable(True)
     return True
 
 def getFullTfName(id, link_name):
+    """
+    Return the full name of the link according to tf tree
+    
+    @param id human id
+    @param link_name name of the link in the tf tree
+    """
     return '/skel/' + id + '/' + link_name
             
 def checkArg(arg):
+    """
+    Approximated a value 
+    to -1 o 1 if the number is not in the range [-1,1]
+    
+    @param arg value to be checked
+    """
     if arg > 1.0: arg = 1.0
     if arg < -1.0: arg = -1.0
     return arg
 
 def getSkeletonTransformation(user_id, tf, tf_name, world_name, last_updated):
+    """
+    Return the rototranslation matrix between tf_name and world_name 
+    and the time stamp
+    
+    @param user_id id of the human
+    @param tf 
+    @param tf_name name of the element in the tf tree
+    @param world_name name of the world in the tf tree
+    @param last_update time stamp of the last time the tf tree was updated
+    """
+    
     tf_name_full = getFullTfName(user_id, tf_name)
     if tf.frameExists(tf_name_full) and tf.frameExists(world_name):   
         try:                
@@ -71,6 +131,11 @@ def getSkeletonTransformation(user_id, tf, tf_name, world_name, last_updated):
         return last_updated, None
     
 def humanList(tf):
+    """
+    return the list of the human id that are in the tf tree 
+    
+    @param tf 
+    """
     import re
     matcher = re.compile('.*user_(\\d+).*')
     all_tfs = tf.getFrameStrings()
@@ -83,6 +148,13 @@ def humanList(tf):
     return all_human_ids
 
 def removeHumans(all_human_ids, humans, env):
+    """
+    Update the list of the human in the env in the basis of all_human_ids
+    
+    @param all_human_ids list of the human id that are in the tf tree 
+    @param humans list of the humans represented in the environment
+    @param env environment
+    """
     # removing
     humans_to_remove = [x for x in humans if not humanInList(x, all_human_ids)]
     for human in humans_to_remove:
